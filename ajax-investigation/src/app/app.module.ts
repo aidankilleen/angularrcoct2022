2fc28f8701fb012ece324db664690e24a4bef6cd
Aidan Killeen <aidan.killeen@gmail.com>
2022-10-14 12:26:30 +0100

    routing investigation

--- ajax-investigation/src/app/app.module.ts
+++ ajax-investigation/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    AboutPageComponent,
-    ContactPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- routing-investigation/src/app/app.component.ts
+++ routing-investigation/src/app/app.component.ts
@@ -2,7 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <h1>Professional Training Website</h1>
+      <nav>
+        <a routerLink="/home" routerLinkActive="active-navigation">Home</a> |
+        <a routerLink="/about" routerLinkActive="active-navigation">About</a> |
+        <a routerLink="/contact" routerLinkActive="active-navigation">Contact Us</a>
+      </nav>
+      <hr>
+
+      <router-outlet></router-outlet>
+
+    </div>
+  
+  
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
